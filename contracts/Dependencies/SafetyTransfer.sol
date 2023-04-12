d973bdc274e61cc92600bf459c9b25cc23045e29
gbirckan <gbirckan@gmail.com>
2023-04-12 20:56:53 -0300

    fixes #25

--- contracts/Dependencies/SafetyTransfer.sol
+++ contracts/Dependencies/SafetyTransfer.sol
@@ -8,16 +8,25 @@ import "../Interfaces/IERC20Decimals.sol";
 library SafetyTransfer {
 	using SafeMathUpgradeable for uint256;
 
+	error EthUnsupportedError();
+	error InvalidAmountError();
+
 	//_amount is in ether (1e18) and we want to convert it to the token decimal
 	function decimalsCorrection(address _token, uint256 _amount) internal view returns (uint256) {
-		if (_token == address(0)) return _amount;
-		if (_amount == 0) return 0;
-
-		uint8 decimals = IERC20Decimals(_token).decimals();
-		if (decimals < 18) {
-			return _amount.div(10**(18 - decimals));
+		if (_token == address(0)) {
+			revert EthUnsupportedError();
 		}
-
+		if (_amount == 0) {
+			return 0;
+		}
+		uint8 decimals = IERC20Decimals(_token).decimals();
+		uint256 divisor = 10**(18 - decimals);
+		if (_amount % divisor != 0) {
+			revert InvalidAmountError();
+		}
+		if (decimals < 18) {
+			return _amount / divisor;
+		}
 		return _amount;
 	}
 }
