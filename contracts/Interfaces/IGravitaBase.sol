7d1c61c3843c7c66e4362ddb20c46028cb8452c4
gbirckan <gbirckan@gmail.com>
2023-04-21 00:41:16 -0300

    fix: rem PoolBase, add upgrade gap to GravBase

--- contracts/Interfaces/IGravitaBase.sol
+++ contracts/Interfaces/IGravitaBase.sol
@@ -5,5 +5,11 @@ pragma solidity 0.8.19;
 import "./IAdminContract.sol";
 
 interface IGravitaBase {
+	struct Colls {
+		// tokens and amounts should be the same length
+		address[] tokens;
+		uint256[] amounts;
+	}
+
 	function adminContract() external view returns (IAdminContract);
 }
