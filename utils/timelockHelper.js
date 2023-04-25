027d619d75407b9b21f68cfe1ecd2e6c17938b52
gbirckan <gbirckan@gmail.com>
2023-04-25 11:01:18 -0300

    feat: add timelock helper (wip)

--- utils/timelockHelper.js
+++ utils/timelockHelper.js
@@ -6,7 +6,8 @@ const TIMELOCK_ADDRESS = "0xb10DeDc5d6bCFDeE354d2817cf634eD83aCEb8FE" // ShortTi
 const TARGET_ADDRESS = "0xA6B8bb6E649E5A587CA5Ec680A71736e32653F3e" // AdminContract
 const METHOD_SIGNATURE = "setMCR(address, uint256)" // setMCR(address _collateral, uint256 newMCR) shortTimelockOnly
 const METHOD_ARG_TYPES = ["address", "uint256"]
-const METHOD_ARG_VALUES = ["0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6", "1100000000000000000"]
+const METHOD_ARG_VALUES = ["0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6", "1150000000000000000"]
+const QUEUED_TX_ETA = "1682383104259200"
 
 main()
 	.then(() => process.exit(0))
@@ -17,57 +18,91 @@ main()
 
 async function main() {
 	await queueTransaction(TARGET_ADDRESS, METHOD_SIGNATURE, METHOD_ARG_TYPES, METHOD_ARG_VALUES)
-	//await executeTransaction(TARGET_ADDRESS, METHOD_SIGNATURE, METHOD_ARG_TYPES, METHOD_ARG_VALUES)
+	// await cancelTransaction(TARGET_ADDRESS, METHOD_SIGNATURE, METHOD_ARG_TYPES, METHOD_ARG_VALUES, QUEUED_TX_ETA)
+	// await executeTransaction(TARGET_ADDRESS, METHOD_SIGNATURE, METHOD_ARG_TYPES, METHOD_ARG_VALUES, QUEUED_TX_ETA)
 }
 
 async function queueTransaction(targetAddress, methodSignature, argTypes, argValues) {
+	console.log(`Queueing ${methodSignature}`)
 	const timelockContract = await getTimelockContract()
 	const eta = await calcETA(timelockContract)
 	const value = 0
 	const data = encodeParameters(argTypes, argValues)
+	const txHash = calcTxHash(targetAddress, value, methodSignature, data, eta)
 
-	const queuedTxHash = ethers.utils.keccak256(
-		encodeParameters(
-			["address", "uint256", "string", "bytes", "uint256"],
-			[targetAddress, value.toString(), methodSignature, data, eta.toString()]
-		)
-	)
+	console.log(`TxHash: ${txHash}`)
+	console.log(`Data: ${data}`)
+	console.log(`ETA: ${eta}`)
 
-	// Timelock::queueTransaction(address target, uint value, string signature, bytes data, uint eta)
+	let queued = await timelockContract.queuedTransactions(txHash)
+	assert(!queued, "Error: Transaction hash already exists in queue!")
+
 	const tx = await timelockContract.queueTransaction(targetAddress, value, methodSignature, data, eta)
 	await tx.wait(1)
 
-	const queued = await timelockContract.queuedTransactions(queuedTxHash)
-	console.log(`QueuedTxHash: ${queuedTxHash} ETA: ${eta} Success: ${queued}`)
+	queued = await timelockContract.queuedTransactions(txHash)
+	console.log(`Success: ${queued}`)
 }
 
-async function executeTransaction(targetAddress, methodSignature, argTypes, argValues) {
-	console.log(`Executing timelocked ${methodSignature}`)
+async function cancelTransaction(targetAddress, methodSignature, argTypes, argValues, queuedTxETA) {
+	console.log(`Canceling queued ${methodSignature}`)
 	const timelockContract = await getTimelockContract()
-	const eta = await getBlockTimestamp()
 	const value = 0
 	const data = encodeParameters(argTypes, argValues)
-	const tx = await timelockContract.executeTransaction(targetAddress, value, methodSignature, data, eta)
+	const txHash = calcTxHash(targetAddress, value, methodSignature, data, queuedTxETA)
+
+	const queued = await timelockContract.queuedTransactions(txHash)
+	assert(!queued, "Transaction does not exist")
+
+	console.log(`TxHash: ${txHash}`)
+	console.log(`Data: ${data}`)
+	console.log(`ETA: ${queuedTxETA}`)
+	const tx = await timelockContract.cancelTransaction(targetAddress, value, methodSignature, data, queuedTxETA)
+	await tx.wait(1)
+
+	const stillQueued = await timelockContract.queuedTransactions(txHash)
+	console.log(`Success: ${!stillQueued}`)
+}
+
+async function executeTransaction(targetAddress, methodSignature, argTypes, argValues, queuedTxETA) {
+	console.log(`Executing queued ${methodSignature}`)
+	const timelockContract = await getTimelockContract()
+	const value = 0
+	const data = encodeParameters(argTypes, argValues)
+	console.log(data)
+	const tx = await timelockContract.executeTransaction(targetAddress, value, methodSignature, data, queuedTxETA)
 	await tx.wait(1)
 	console.log(`Transaction executed`)
 }
 
 async function getTimelockContract() {
-	const deployerWallet = new ethers.Wallet(DEPLOYER_PRIVATEKEY, ethers.provider)
-	return await ethers.getContractAt("Timelock", TIMELOCK_ADDRESS, deployerWallet)
+	const wallet = new ethers.Wallet(DEPLOYER_PRIVATEKEY, ethers.provider)
+	const balance = await ethers.provider.getBalance(wallet.address)
+	console.log(`Using wallet ${wallet.address} [Balance: ${ethers.utils.formatUnits(balance)}]`)
+	return await ethers.getContractAt("Timelock", TIMELOCK_ADDRESS, wallet)
 }
 
 async function calcETA(timelockContract) {
-	const delay = await timelockContract.delay()
+	const delay = Number(await timelockContract.delay())
 	return (await getBlockTimestamp()) + delay
 }
 
 async function getBlockTimestamp() {
 	const currentBlock = await ethers.provider.getBlockNumber()
-	return (await ethers.provider.getBlock(currentBlock)).timestamp
+	return Number((await ethers.provider.getBlock(currentBlock)).timestamp)
 }
 
 function encodeParameters(types, values) {
 	const abi = new ethers.utils.AbiCoder()
 	return abi.encode(types, values)
 }
+
+function calcTxHash(targetAddress, value, methodSignature, data, eta) {
+	return ethers.utils.keccak256(
+		encodeParameters(
+			["address", "uint256", "string", "bytes", "uint256"],
+			[targetAddress, value.toString(), methodSignature, data, eta.toString()]
+		)
+	)
+}
+
