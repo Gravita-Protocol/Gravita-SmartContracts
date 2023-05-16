2a7bc5fd62cb19c481ca17acc8fd465f0a1610e9
Fornax <23104993+fornax2@users.noreply.github.com>
2023-05-16 14:08:45 -0300

    New addresses.

--- contracts/Addresses.sol
+++ contracts/Addresses.sol
@@ -8,7 +8,7 @@ contract Addresses {
 	address public constant borrowerOperations = 0x2bCA0300c2aa65de6F19c2d241B54a445C9990E2;
 	address public constant collSurplusPool = 0x09dfdF392a56E4316e97A13e20b09C415fCD3d7b;
 	address public constant communityIssuance = address(0);
-	address public constant debtToken = address(0);
+	address public constant debtToken = 0x15f74458aE0bFdAA1a96CA1aa779D715Cc1Eefe4;
 	address public constant defaultPool = 0x84446698694B348EaeCE187b55df06AB4Ce72b35;
 	address public constant feeCollector = 0x4928c8F8c20A1E3C295DddBe05095A9aBBdB3d14;
 	address public constant gasPoolAddress = 0x40E0e274A42D9b1a9D4B64dC6c46D21228d45C20;
@@ -16,7 +16,7 @@ contract Addresses {
 	address public constant priceFeed = 0x89F1ecCF2644902344db02788A790551Bb070351;
 	address public constant sortedVessels = 0xF31D88232F36098096d1eB69f0de48B53a1d18Ce;
 	address public constant stabilityPool = 0x4F39F12064D83F6Dd7A2BDb0D53aF8be560356A6;
-	address public constant timelockAddress = address(0);
+	address public constant timelockAddress = 0x57a1953bF194A1EF73396e442Ac7Dc761dCd23cc;
 	address public constant treasuryAddress = 0x6F8Fe995422c5efE6487A7B07f67E84aaD9D4eC8;
 	address public constant vesselManager = 0xdB5DAcB1DFbe16326C3656a88017f0cB4ece0977;
 	address public constant vesselManagerOperations = 0xc49B737fa56f9142974a54F6C66055468eC631d0;
