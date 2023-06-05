0c756415323a794481fb8935f7e4381394b7f6be
gbirckan <gbirckan@gmail.com>
2023-06-05 17:15:01 -0300

    update upgrade proposal script

--- scripts/upgrades/propose-upgrade.js
+++ scripts/upgrades/propose-upgrade.js
@@ -1,10 +1,12 @@
 const { defender } = require("hardhat")
 
+const multisig = "0x4828528085FA6a14408fe5b81934481f5397a3F3"
+
 async function main() {
 	const proxyAddress = "0x655D09c912248fa6fE55657633BA4B92a1F9be1F"
 	const NewContract = await ethers.getContractFactory("PriceFeed")
 	console.log("Preparing proposal...")
-	const proposal = await defender.proposeUpgrade(proxyAddress, NewContract)
+	const proposal = await defender.proposeUpgrade(proxyAddress, NewContract, { multisig })
 	console.log("Upgrade proposal created at:", proposal.url)
 }
 
