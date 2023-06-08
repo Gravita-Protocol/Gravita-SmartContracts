e7a47b9432032397af412b3a6dc12c870cdc8e86
gbirckan <gbirckan@gmail.com>
2023-06-08 16:37:49 -0300

    fallback tests

--- test/gravita/PriceFeedTest.js
+++ test/gravita/PriceFeedTest.js
@@ -7,7 +7,6 @@ const {
 const { ZERO_ADDRESS } = require("@openzeppelin/test-helpers/src/constants")
 const { assert } = require("hardhat")
 
-const AdminContract = artifacts.require("AdminContract")
 const ERC20Mock = artifacts.require("ERC20Mock")
 const FixedPriceAggregator = artifacts.require("FixedPriceAggregator")
 const MockChainlink = artifacts.require("MockAggregator")
@@ -20,11 +19,11 @@ const WstEth2UsdPriceAggregator = artifacts.require("WstEth2UsdPriceAggregator")
 const { TestHelper } = require("../utils/testHelpers.js")
 const { dec, assertRevert, toBN, getLatestBlockTimestamp, fastForwardTime } = TestHelper
 
-const DEFAULT_PRICE = dec(100, 18)
-const DEFAULT_PRICE_e8 = dec(100, 8)
+const DEFAULT_DIGITS = 18
+const DEFAULT_PRICE = dec(100, DEFAULT_DIGITS)
 
 const DefaultOracleOptions = {
-	providerType: 0, // enum IPriceFeed.ProviderType
+	providerType: 0, // IPriceFeed.sol::enum ProviderType.Chainlink
 	timeoutMinutes: 60,
 	isEthIndexed: false,
 	isFallback: false,
@@ -33,77 +32,65 @@ const DefaultOracleOptions = {
 contract("PriceFeed", async accounts => {
 	const [owner, alice] = accounts
 	let priceFeed
-	let mockChainlink
-	let adminContract
+	let mockOracle
 	let timelock
 	let erc20
 
-	const setAddressesAndOracle = async () => {
-		await priceFeed.setTimelock(timelock.address)
-		await setOracle(ZERO_ADDRESS, mockChainlink.address)
-	}
-
-	const setOracle = async (erc20Address, aggregatorAddress, opt = DefaultOracleOptions) => {
-		const record = await priceFeed.oracles(erc20Address)
-		if (record.decimals == 0) {
+	const setOracle = async (erc20Address, aggregatorAddress, opts = DefaultOracleOptions) => {
+		const callSetter = async (_token, _oracle, _opts, _caller) => {
 			await priceFeed.setOracle(
-				erc20Address,
-				aggregatorAddress,
-				opt.providerType,
-				opt.timeoutMinutes,
-				opt.isEthIndexed,
-				opt.isFallback,
+				_token,
+				_oracle,
+				_opts.providerType,
+				_opts.timeoutMinutes,
+				_opts.isEthIndexed,
+				_opts.isFallback,
 				{
-					from: owner,
+					from: _caller,
 				}
 			)
+		}
+		const record = opts.isFallback ? await priceFeed.fallbacks(erc20Address) : await priceFeed.oracles(erc20Address)
+		const recordExists = record.decimals != 0
+		if (!recordExists) {
+			await callSetter(erc20Address, aggregatorAddress, opts, owner)
 		} else {
+			// updating existing record requires msg.caller to be the timelock
+			console.log(`calling as timelock`)
 			await impersonateAccount(timelock.address)
-			await priceFeed.setOracle(
-				erc20Address,
-				aggregatorAddress,
-				opt.providerType,
-				opt.timeoutMinutes,
-				opt.isEthIndexed,
-				opt.isFallback,
-				{
-					from: timelock.address,
-				}
-			)
+			await callSetter(erc20Address, aggregatorAddress, opts, timelock.address)
 			await stopImpersonatingAccount(timelock.address)
 		}
 	}
 
+	const createMockOracle = async (_price, _decimals) => {
+		const oracle = await MockChainlink.new()
+		await oracle.setDecimals(_decimals)
+		await oracle.setPrice(_price)
+		await oracle.setLatestRoundId(3)
+		await oracle.setPrevRoundId(2)
+		await oracle.setUpdateTime(await time.latest())
+		return oracle
+	}
+
 	beforeEach(async () => {
 		priceFeed = await PriceFeed.new()
-		mockChainlink = await MockChainlink.new()
-		adminContract = await AdminContract.new()
-		erc20 = await ERC20Mock.new("MOCK", "MOCK", 18)
+		mockOracle = await createMockOracle(DEFAULT_PRICE, DEFAULT_DIGITS)
+		erc20 = await ERC20Mock.new("MOCK", "MOCK", DEFAULT_DIGITS)
 
 		await priceFeed.initialize()
 
 		timelock = await Timelock.new(86400 * 2, owner)
 		setBalance(timelock.address, 1e18)
 
-		// Set Chainlink latest and prev roundId's to non-zero
-		await mockChainlink.setLatestRoundId(3)
-		await mockChainlink.setPrevRoundId(2)
-
-		// Set current and prev prices
-		await mockChainlink.setPrice(DEFAULT_PRICE_e8)
-		await mockChainlink.setPrevPrice(DEFAULT_PRICE_e8)
-
-		await mockChainlink.setDecimals(8)
-
-		// Set mock price updateTimes to very recent
-		const now = await getLatestBlockTimestamp(web3)
-		await mockChainlink.setUpdateTime(now)
+		await priceFeed.setTimelock(timelock.address)
+		await setOracle(ZERO_ADDRESS, mockOracle.address)
 	})
 
 	describe("PriceFeedTestnet: internal testing contract", async () => {
 		it("should be able to fetchPrice after setPrice, output of former matching input of latter", async () => {
 			const priceFeedTestnet = await PriceFeedTestnet.new()
-			const targetPrice = dec(1_000, 18)
+			const targetPrice = dec(1_000, DEFAULT_DIGITS)
 			await priceFeedTestnet.setPrice(ZERO_ADDRESS, targetPrice)
 			const price = await priceFeedTestnet.getPrice(ZERO_ADDRESS)
 			assert.equal(price, targetPrice)
@@ -112,11 +99,10 @@ contract("PriceFeed", async accounts => {
 
 	describe("setOracle() routines", async () => {
 		it("setOracle as user, reverts", async () => {
-			await setAddressesAndOracle()
 			await assertRevert(
 				priceFeed.setOracle(
 					ZERO_ADDRESS,
-					mockChainlink.address,
+					mockOracle.address,
 					DefaultOracleOptions.providerType,
 					DefaultOracleOptions.timeoutMinutes,
 					DefaultOracleOptions.isEthIndexed,
@@ -129,71 +115,63 @@ contract("PriceFeed", async accounts => {
 		})
 
 		it("setOracle as timelock: broken oracle, reverts", async () => {
-			await setAddressesAndOracle()
-			await mockChainlink.setLatestRoundId(0)
-			await assertRevert(setOracle(ZERO_ADDRESS, mockChainlink.address))
+			await mockOracle.setLatestRoundId(0)
+			await assertRevert(setOracle(ZERO_ADDRESS, mockOracle.address))
+		})
+
+		it("setOracle as timelock: set fallback with no primary oracle, reverts", async () => {
+			const newErc20Address = ethers.Wallet.createRandom().address
+			await assertRevert(setOracle(newErc20Address, mockOracle.address, { ...DefaultOracleOptions, isFallback: true }))
 		})
 
 		it("setOracle as timelock: response is good, adds new oracle", async () => {
-			await setAddressesAndOracle()
 			const price = await priceFeed.fetchPrice(ZERO_ADDRESS)
 			assert.equal(price.toString(), DEFAULT_PRICE.toString())
 		})
 
 		it("setOracle as timelock: oracle update, replaces previous one", async () => {
-			await setAddressesAndOracle()
 			const price = await priceFeed.fetchPrice(ZERO_ADDRESS)
 			assert.equal(price.toString(), DEFAULT_PRICE.toString())
 
-			const newMockChainlink = await MockChainlink.new()
-			MockChainlink.setAsDeployed(newMockChainlink)
-			await newMockChainlink.setPrice(dec(2_345, 8))
-			await newMockChainlink.setPrevPrice(dec(2_345, 8))
-			await newMockChainlink.setLatestRoundId(3)
-			await newMockChainlink.setPrevRoundId(2)
-			await newMockChainlink.setDecimals(8)
-			await newMockChainlink.setUpdateTime(await time.latest())
-
-			await setOracle(ZERO_ADDRESS, newMockChainlink.address)
+			const newMockOracle = await createMockOracle(dec(2_345, 8), 8)
+			await setOracle(ZERO_ADDRESS, newMockOracle.address)
 
 			const newPrice = await priceFeed.fetchPrice(ZERO_ADDRESS)
-			assert.equal(newPrice.toString(), dec(2_345, 18).toString())
+			assert.equal(newPrice.toString(), dec(2_345, DEFAULT_DIGITS).toString())
 		})
 	})
 
 	describe("Custom Aggregators", async () => {
 		it("fetchPrice of ETH-indexed oracle", async () => {
-			await setAddressesAndOracle()
-
-			const ETH_TO_USD = dec(1_600, 18)
-			const ethMockChainlink = await MockChainlink.new()
-			await ethMockChainlink.setPrevPrice(ETH_TO_USD)
-			await ethMockChainlink.setPrice(ETH_TO_USD)
-			await ethMockChainlink.setDecimals(18)
-			await ethMockChainlink.setLatestRoundId(3)
-			await ethMockChainlink.setPrevRoundId(2)
-			await ethMockChainlink.setUpdateTime(await getLatestBlockTimestamp(web3))
+			const ETH_TO_USD = dec(1_600, DEFAULT_DIGITS)
+			const ethMockOracle = await createMockOracle(ETH_TO_USD, DEFAULT_DIGITS)
 
 			const ERC20_TO_ETH = dec(11, 17) // ERC20:ETH = 1,1
-			const erc20MockChainlink = await MockChainlink.new()
-			await erc20MockChainlink.setPrice(ERC20_TO_ETH)
-			await erc20MockChainlink.setPrevPrice(ERC20_TO_ETH)
-			await erc20MockChainlink.setLatestRoundId(3)
-			await erc20MockChainlink.setPrevRoundId(2)
-			await erc20MockChainlink.setDecimals(18)
-			await erc20MockChainlink.setUpdateTime(await getLatestBlockTimestamp(web3))
+			const erc20MockOracle = await createMockOracle(ERC20_TO_ETH, DEFAULT_DIGITS)
 
-			await setOracle(ZERO_ADDRESS, ethMockChainlink.address)
-			await setOracle(erc20.address, erc20MockChainlink.address, { ...DefaultOracleOptions, isEthIndexed: true })
-			
+			await setOracle(ZERO_ADDRESS, ethMockOracle.address)
+			await setOracle(erc20.address, erc20MockOracle.address, { ...DefaultOracleOptions, isEthIndexed: true })
+
 			const erc20Price = await priceFeed.fetchPrice(erc20.address)
 			const expectedPrice = toBN(ERC20_TO_ETH).mul(toBN(ETH_TO_USD)).div(toBN(1e18))
 			assert.equal(erc20Price.toString(), expectedPrice.toString())
 		})
 
 		it("fetchPrice of ETH-indexed oracle, where aggregator's response digits are different", async () => {
-			// TODO
-			assert.equal(true, false)
+			const ETH_TO_USD = dec(1_600, 12) // ETH:USD = 1,600 @ 12 digits
+			const ethMockOracle = await createMockOracle(ETH_TO_USD, 12)
+
+			const ERC20_TO_ETH = dec(11, 7) // ERC20:ETH = 1,1 @ 8 digits
+			const erc20MockOracle = await createMockOracle(ERC20_TO_ETH, 8)
+
+			await setOracle(ZERO_ADDRESS, ethMockOracle.address)
+			await setOracle(erc20.address, erc20MockOracle.address, { ...DefaultOracleOptions, isEthIndexed: true })
+
+			const erc20Price = await priceFeed.fetchPrice(erc20.address) // ERC20:USD response @ 18 digits
+			const expectedPrice = toBN(11)
+				.mul(toBN(1_600))
+				.mul(toBN(10 ** 17))
+			assert.equal(erc20Price.toString(), expectedPrice.toString())
 		})
 
 		it("fetchPrice of unknown token, reverts", async () => {
@@ -202,7 +180,6 @@ contract("PriceFeed", async accounts => {
 		})
 
 		it("fixed price aggregator", async () => {
-			await setAddressesAndOracle()
 			const one_to_one_oracle = await FixedPriceAggregator.new(1e8)
 			await priceFeed.setOracle(
 				erc20.address,
@@ -220,22 +197,14 @@ contract("PriceFeed", async accounts => {
 			const WSTETH_TO_STETH = "1124168697480543467"
 			const STETH_TO_USD = "182673073369"
 
-			await setAddressesAndOracle()
-
-			const stEth_to_usd_mockChainlink = await MockChainlink.new()
-			await stEth_to_usd_mockChainlink.setDecimals(8)
-			await stEth_to_usd_mockChainlink.setPrice(STETH_TO_USD)
-			await stEth_to_usd_mockChainlink.setPrevPrice(STETH_TO_USD)
-			await stEth_to_usd_mockChainlink.setLatestRoundId(3)
-			await stEth_to_usd_mockChainlink.setPrevRoundId(2)
-			await stEth_to_usd_mockChainlink.setUpdateTime(await getLatestBlockTimestamp(web3))
+			const stEth_to_usd_mockOracle = await createMockOracle(STETH_TO_USD, 8)
 
 			const mock_wstETH = await MockWstETH.new()
 			mock_wstETH.setStETHPerToken(WSTETH_TO_STETH)
 
 			const wstEth_to_usd_oracle = await WstEth2UsdPriceAggregator.new(
 				mock_wstETH.address,
-				stEth_to_usd_mockChainlink.address
+				stEth_to_usd_mockOracle.address
 			)
 			const aggregatorDecimals = Number(await wstEth_to_usd_oracle.decimals())
 			assert.equal(aggregatorDecimals, 8)
@@ -262,7 +231,7 @@ contract("PriceFeed", async accounts => {
 				DefaultOracleOptions.isFallback
 			)
 			const feedDigits = Number(await priceFeed.TARGET_DIGITS())
-			assert.equal(feedDigits, 18)
+			assert.equal(feedDigits, DEFAULT_DIGITS)
 
 			const wstEth_to_usd_priceBN2 = await priceFeed.fetchPrice(mock_wstETH.address)
 			const wstEth_to_usd_price2 = ethers.utils.formatUnits(wstEth_to_usd_priceBN2.toString(), feedDigits)
@@ -273,57 +242,43 @@ contract("PriceFeed", async accounts => {
 
 	describe("fetchPrice() scaling up/down", async () => {
 		it("Scaling aggregator's response up", async () => {
-			await setAddressesAndOracle()
-
-			// Oracle price price is 10, in 8 digits (10 * 10**8)
-			await mockChainlink.setDecimals(8)
-			await mockChainlink.setPrice(dec(10, 8))
-			await mockChainlink.setPrevPrice(dec(10, 8))
-			let price = await priceFeed.fetchPrice(ZERO_ADDRESS)
-			// Check PriceFeed gives 10, with 18 digit precision
-			assert.equal(price, dec(10, 18))
-
 			// Oracle price is 1, in 12 digits
-			let newErc20 = ethers.Wallet.createRandom().address
-			await mockChainlink.setDecimals(12)
-			await mockChainlink.setPrice(dec(1, 12))
-			await mockChainlink.setPrevPrice(dec(1, 12))
-			await setOracle(newErc20, mockChainlink.address)
-			price = await priceFeed.fetchPrice(newErc20)
+			let newErc20Address = ethers.Wallet.createRandom().address
+			await mockOracle.setDecimals(12)
+			await mockOracle.setPrice(dec(1, 12))
+			await setOracle(newErc20Address, mockOracle.address)
+			price = await priceFeed.fetchPrice(newErc20Address)
 			// Check PriceFeed gives 1e12, but with 18 digit precision (scale up)
-			assert.equal(price.toString(), dec(1, 18))
+			assert.equal(price.toString(), dec(1, DEFAULT_DIGITS))
 
 			// Oracle price is 1234.56789, in 5 digits
-			newErc20 = ethers.Wallet.createRandom().address
-			await mockChainlink.setDecimals(5)
-			await mockChainlink.setPrice(dec(123_456_789))
-			await mockChainlink.setPrevPrice(dec(123_456_789))
-			await setOracle(newErc20, mockChainlink.address)
-			price = await priceFeed.fetchPrice(newErc20)
+			newErc20Address = ethers.Wallet.createRandom().address
+			await mockOracle.setDecimals(5)
+			await mockOracle.setPrice(dec(123_456_789))
+			await setOracle(newErc20Address, mockOracle.address)
+			price = await priceFeed.fetchPrice(newErc20Address)
 			// Check PriceFeed gives 1234.56789 with 18 digit precision (scale up)
 			assert.equal(price, "1234567890000000000000")
 		})
 
 		it("No scaling necessary (aggregator uses 18 digits)", async () => {
 			// Oracle price is 0.0001, in 18 digits
-			const newErc20 = ethers.Wallet.createRandom().address
-			await mockChainlink.setDecimals(18)
-			await mockChainlink.setPrice(dec(1, 14))
-			await mockChainlink.setPrevPrice(dec(1, 14))
-			await setOracle(newErc20, mockChainlink.address)
-			const price = await priceFeed.fetchPrice(newErc20)
+			const newErc20Address = ethers.Wallet.createRandom().address
+			await mockOracle.setDecimals(18)
+			await mockOracle.setPrice(dec(1, 14))
+			await setOracle(newErc20Address, mockOracle.address)
+			const price = await priceFeed.fetchPrice(newErc20Address)
 			// Check PriceFeed gives 0.0001 with 18 digit precision (no scale, 18 is default)
 			assert.equal(price.toString(), dec(1, 14))
 		})
 
 		it("Scaling aggregator's response down", async () => {
 			// Oracle price is 1, in 20 digits
-			const newErc20 = ethers.Wallet.createRandom().address
-			await mockChainlink.setDecimals(20)
-			await mockChainlink.setPrice(dec(1, 20))
-			await mockChainlink.setPrevPrice(dec(1, 20))
-			await setOracle(newErc20, mockChainlink.address)
-			const price = await priceFeed.fetchPrice(newErc20)
+			const newErc20Address = ethers.Wallet.createRandom().address
+			await mockOracle.setDecimals(20)
+			await mockOracle.setPrice(dec(1, 20))
+			await setOracle(newErc20Address, mockOracle.address)
+			const price = await priceFeed.fetchPrice(newErc20Address)
 			// Check PriceFeed gives 1 with 18 digit precision (scale down)
 			assert.equal(price.toString(), dec(1, 18))
 		})
@@ -331,13 +286,33 @@ contract("PriceFeed", async accounts => {
 
 	describe("fetchPrice() fallbacks", async () => {
 		it("fetchPrice: oracle is stale, no fallback, reverts", async () => {
-			await setAddressesAndOracle()
-			await mockChainlink.setPrice(dec(1_234, 8))
-			await mockChainlink.setPrevPrice(dec(1_234, 8))
 			await time.increase(DefaultOracleOptions.timeoutMinutes * 60 + 1)
 			assertRevert(priceFeed.fetchPrice(ZERO_ADDRESS))
 		})
-		// TODO test all fallback scenarios
+
+		it("fetchPrice: oracle is stale, fallback is good, returns fallback response", async () => {
+			const fallbackPrice = DEFAULT_PRICE * 2
+			const fallbackOracle = await createMockOracle(fallbackPrice.toString(), DEFAULT_DIGITS)
+			await setOracle(ZERO_ADDRESS, fallbackOracle.address, { ...DefaultOracleOptions, isFallback: true })
+
+			await mockOracle.setPriceIsAlwaysUpToDate(false)
+			await time.increase(DefaultOracleOptions.timeoutMinutes * 60 + 30)
+
+			fallbackOracle.setUpdateTime(await time.latest())
+			const feedPrice = await priceFeed.fetchPrice(ZERO_ADDRESS)
+			assert.equal(fallbackPrice.toString(), feedPrice.toString())
+		})
+
+		it("fetchPrice: oracle is stale, fallback is stale, reverts", async () => {
+			const fallbackOracle = await createMockOracle(DEFAULT_PRICE.toString(), DEFAULT_DIGITS)
+			await setOracle(ZERO_ADDRESS, fallbackOracle.address, { ...DefaultOracleOptions, isFallback: true })
+
+			await mockOracle.setPriceIsAlwaysUpToDate(false)
+			await fallbackOracle.setPriceIsAlwaysUpToDate(false)
+			await time.increase(DefaultOracleOptions.timeoutMinutes * 60 + 30)
+
+			assertRevert(priceFeed.fetchPrice(ZERO_ADDRESS))
+		})
 	})
 })
 
