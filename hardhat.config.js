c77f0461951c883b6fe77ab3758ad303b3659ffb
grok13 <>
2023-06-26 15:52:05 +0200

    fix: typos

--- hardhat.config.js
+++ hardhat.config.js
@@ -53,6 +53,20 @@ module.exports = {
 					},
 				},
 			},
+			{
+				version: "0.6.12",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+					outputSelection: {
+						"*": {
+							"*": ["storageLayout"],
+						},
+					},
+				},
+			},
 		],
 	},
 	networks: {
