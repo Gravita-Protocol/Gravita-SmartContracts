51477fc9607c320e9f7c2eb2749d8fea4db907e5
gbirckan <gbirckan@gmail.com>
2023-07-24 17:29:57 -0300

    wip

---
+++ contracts/Interfaces/IRewardAccruing.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+/**
+ * @notice Interface to be implemented by contracts that wrap reward-accruing ERC20s (such as ConvextStakingWrapper.sol).
+ */
+interface IRewardAccruing {
+
+  event RewardAccruingRightsTransferred(address _from, address _to, uint256 _amount);
+
+  function transferRewardAccruingRights(address _from, address _to, uint256 _amount) external;
+}
