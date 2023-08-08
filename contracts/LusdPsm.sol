f95e501b068ecddeffa0176ade0a7ce713ae82a1
Fornax <23104993+fornax2@users.noreply.github.com>
2023-08-08 17:13:16 -0300

    Adding checks.

--- contracts/LusdPsm.sol
+++ contracts/LusdPsm.sol
@@ -12,10 +12,10 @@ import { IPool } from "lib/aave-v3-core/contracts/interfaces/IPool.sol";
 import "./Dependencies/SafetyTransfer.sol";
 import "./Addresses.sol";
 
-contract LUSDPSM is UUPSUpgradeable, OwnableUpgradeable, Addresses, ReentrancyGuardUpgradeable {
+contract LusdPsm is UUPSUpgradeable, OwnableUpgradeable, Addresses, ReentrancyGuardUpgradeable {
 	using SafeERC20Upgradeable for IERC20Upgradeable;
 
-	string public constant NAME = "LUSDPSM";
+	string public constant NAME = "LusdPsm";
 
 	uint256 public buyFee;
 	uint256 public sellFee;
@@ -25,50 +25,70 @@ contract LUSDPSM is UUPSUpgradeable, OwnableUpgradeable, Addresses, ReentrancyGu
 
 	event BuyLUSD(address indexed user, uint256 amount, uint256 fee);
 	event SellLUSD(address indexed user, uint256 amount, uint256 fee);
-	event NewBuyFee(uint256 fee);
-	event NewSellFee(uint256 fee);
+	event BuyFeeChanged(uint256 fee);
+	event SellFeeChanged(uint256 fee);
+
+	error LusdPsm__InvalidAddress();
+	error LusdPsm__InvalidAmount();
 
 	constructor(address _lusd, address _pool) {
+		if (_lusd == address(0) || _pool == address(0)) {
+			revert LusdPsm__InvalidAddress();
+		}
 		lusd = _lusd;
 		aavePool = IPool(_pool);
 	}
 
 	// --- Initializer ---
-	function initialize() public initializer {
+	function initialize(uint256 _buyFee, uint256 _sellFee) public initializer {
 		__Ownable_init();
 		__UUPSUpgradeable_init();
 		__ReentrancyGuard_init();
+
+		buyFee = _buyFee;
+		sellFee = _sellFee;
 	}
 
 	function sellLUSD(uint256 _lusdAmount) public nonReentrant {
-		uint256 fee = (_lusdAmount * sellFee) / DECIMAL_PRECISION;
-		uint256 graiAmount = _lusdAmount - fee;
+		if (_lusdAmount == 0) {
+			revert LusdPsm__InvalidAmount();
+		}
+		uint256 feeAmount = (_lusdAmount * sellFee) / DECIMAL_PRECISION;
+		uint256 graiAmount = _lusdAmount - feeAmount;
 		IERC20Upgradeable(lusd).transferFrom(msg.sender, address(this), _lusdAmount);
+		IERC20Upgradeable(lusd).transfer(treasuryAddress, feeAmount);
 		aavePool.supply(lusd, _lusdAmount, address(this), 0);
 
 		IDebtToken(debtToken).mintFromWhitelistedContract(graiAmount);
 		IERC20Upgradeable(debtToken).safeTransferFrom(address(this), msg.sender, graiAmount);
-		emit SellLUSD(msg.sender, _lusdAmount, fee);
+		emit SellLUSD(msg.sender, _lusdAmount, feeAmount);
 	}
 
 	function buyLUSD(uint256 _lusdAmount) public nonReentrant {
-		uint256 fee = (_lusdAmount * buyFee) / DECIMAL_PRECISION;
-		uint256 graiAmount = _lusdAmount + fee;
+		if (_lusdAmount == 0) {
+			revert LusdPsm__InvalidAmount();
+		}
+		uint256 feeAmount = (_lusdAmount * buyFee) / DECIMAL_PRECISION;
+		uint256 graiAmount = _lusdAmount + feeAmount;
 		IERC20Upgradeable(debtToken).transferFrom(msg.sender, address(this), graiAmount);
-		IERC20Upgradeable(debtToken).transfer(treasuryAddress, fee);
-		IDebtToken(debtToken).burnFromWhitelistedContract(graiAmount - fee);
+		IERC20Upgradeable(debtToken).transfer(treasuryAddress, feeAmount);
+		IDebtToken(debtToken).burnFromWhitelistedContract(_lusdAmount);
 		aavePool.withdraw(lusd, _lusdAmount, msg.sender);
-		emit BuyLUSD(msg.sender, _lusdAmount, fee);
+		emit BuyLUSD(msg.sender, _lusdAmount, feeAmount);
 	}
 
 	function setBuyFee(uint256 _fee) public onlyOwner {
 		buyFee = _fee;
-		emit NewBuyFee(_fee);
+		emit BuyFeeChanged(_fee);
 	}
 
 	function setSellFee(uint256 _fee) public onlyOwner {
 		sellFee = _fee;
-		emit NewSellFee(_fee);
+		emit SellFeeChanged(_fee);
+	}
+
+	function withdrawExcessFromDeposits(uint256 _amount) public onlyOwner {
+		aavePool.withdraw(lusd, _amount, treasuryAddress);
 	}
 
 	function authorizeUpgrade(address newImplementation) public {
