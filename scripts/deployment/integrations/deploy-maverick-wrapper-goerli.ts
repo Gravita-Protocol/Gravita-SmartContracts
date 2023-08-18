d0b514ab79e8c2d716c53256c20b59a2eef320bf
gbirckan <gbirckan@gmail.com>
2023-08-18 14:15:26 -0300

    complement for wrapper deploy script

--- scripts/deployment/integrations/deploy-maverick-wrapper-goerli.ts
+++ scripts/deployment/integrations/deploy-maverick-wrapper-goerli.ts
@@ -4,19 +4,39 @@ const { hre, ethers, upgrades } = require("hardhat")
 // Goerli Setup
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const lpToken = "0xC3408e89622Bf7F590497F2CADf05DfeCaaF7DfB" // Maverick Position-WBTC-USDC-15
-const rewards = "0x1cc742CBD730cAd9E121eC9b95D08E066E6A9780"
+const lpTokenAddress = "0xC3408e89622Bf7F590497F2CADf05DfeCaaF7DfB" // Maverick Position-WBTC-USDC-15
+const rewardsAddress = "0x1cc742CBD730cAd9E121eC9b95D08E066E6A9780"
+const adminContractAddress = "0x0f3F21BBA51f6903C86048BfD1087051BA0D5658"
 
 async function main() {
 	const contractName = `MaverickStakingWrapper`
 	console.log(`Deploying ${contractName}...`)
 	const factory = await ethers.getContractFactory(contractName)
 	let opts: any = { kind: "uups", initializer: false }
-	const newContract = await upgrades.deployProxy(factory, opts)
-	await newContract.deployed()
-  console.log(`Contract deployed at ${newContract.address}, initializing...`)
-	await newContract.initialize(lpToken, rewards)
-  console.log(`Done.`)
+	const wrapper = await upgrades.deployProxy(factory, opts)
+	await wrapper.deployed()
+	console.log(`Contract deployed at ${wrapper.address}, initializing...`)
+	await wrapper.initialize(lpTokenAddress, rewardsAddress)
+	console.log(`Contract initialized, setting addresses...`)
+	const adminContract = await ethers.getContractAt("AdminContract", adminContractAddress)
+	await wrapper.setAddresses([
+		await adminContract.activePool(),
+		adminContractAddress,
+		await adminContract.borrowerOperations(),
+		await adminContract.collSurplusPool(),
+		await adminContract.debtToken(),
+		await adminContract.defaultPool(),
+		await adminContract.feeCollector(),
+		await adminContract.gasPoolAddress(),
+		await adminContract.priceFeed(),
+		await adminContract.sortedVessels(),
+		await adminContract.stabilityPool(),
+		await adminContract.timelockAddress(),
+		await adminContract.treasuryAddress(),
+		await adminContract.vesselManager(),
+		await adminContract.vesselManagerOperations(),
+	])
+	console.log(`Done.`)
 }
 
 main()
@@ -25,3 +45,4 @@ main()
 		console.error(error)
 		process.exit(1)
 	})
+
