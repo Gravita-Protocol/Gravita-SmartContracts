01fec19e7e5cbb11551a81b4c8c5100a0e5cd67e
gbirckan <gbirckan@gmail.com>
2023-10-18 16:55:29 -0300

    VesselMgrOps upgrade

--- scripts/upgrades/test-upgrade-from-dev-wallet.js
+++ scripts/upgrades/test-upgrade-from-dev-wallet.js
@@ -2,12 +2,8 @@ const { run, upgrades: upgrades2 } = require("hardhat")
 
 const upgrades = [
 	{
-		contract: "FeeCollector",
-		address: "0x9c966245C17F953Fa1e6FCaD8E79B0D7f9d2a872",
-	},
-	{
-		contract: "VesselManager",
-		address: "0x2177946fD433F24666b23c51C8D728c34Af05627",
+		contract: "VesselManagerOperations",
+		address: "0x4FC9067e08B16293b6aB251bB335e832F0e896C9",
 	},
 ]
 
