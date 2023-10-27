9194e9e157c958779232373ec2d96ebae5aff736
gbirckan <gbirckan@gmail.com>
2023-10-27 18:51:57 -0300

    VesselManager contract upgrade

--- scripts/upgrades/propose-upgrade.js
+++ scripts/upgrades/propose-upgrade.js
@@ -1,14 +1,18 @@
 const { defender } = require("hardhat")
 
-const targetUpgrade = "VesselManagerOperations"
-const targetAddress = "0xc49B737fa56f9142974a54F6C66055468eC631d0"
+const targetUpgrade = "VesselManager"
+const targetAddress = "0xdB5DAcB1DFbe16326C3656a88017f0cB4ece0977"
 const multisig = "0xE9Ac7a720C3511fD048a47f148066B0479102234"
 
 async function main() {
+	const wallet = new ethers.Wallet(process.env.DEPLOYER_PRIVATEKEY, ethers.provider)
+	const balance = await ethers.provider.getBalance(wallet.address)
+	console.log(`Using wallet ${wallet.address} [Balance: ${ethers.utils.formatUnits(balance)}]`)
 	const newContractVersion = await ethers.getContractFactory(targetUpgrade)
-	console.log("Preparing proposal...")
+	console.log(`Preparing proposal for ${targetUpgrade}...`)
 	const proposal = await defender.proposeUpgrade(targetAddress, newContractVersion, { multisig })
 	console.log("Upgrade proposal created at:", proposal.url)
+	console.log(proposal)
 }
 
 main()
