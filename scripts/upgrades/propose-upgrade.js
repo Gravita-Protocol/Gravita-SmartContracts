a9b3ecac8371f22ea0de79ae6f120771d2409908
gbirckan <gbirckan@gmail.com>
2023-10-29 17:30:51 -0300

    VesselManager contract upgrade (Arbitrum)

--- scripts/upgrades/propose-upgrade.js
+++ scripts/upgrades/propose-upgrade.js
@@ -1,8 +1,11 @@
 const { defender } = require("hardhat")
 
 const targetUpgrade = "VesselManager"
-const targetAddress = "0xdB5DAcB1DFbe16326C3656a88017f0cB4ece0977"
-const multisig = "0xE9Ac7a720C3511fD048a47f148066B0479102234"
+// const targetAddress = "0xdB5DAcB1DFbe16326C3656a88017f0cB4ece0977" // Mainnet VesselManager
+// const multisig = "0xE9Ac7a720C3511fD048a47f148066B0479102234" // Mainnet Upgrades Multisig
+
+const targetAddress = "0x6AdAA3eBa85c77e8566b73AEfb4C2f39Df4046Ca" // Arbitrum VesselManager
+const multisig = "0xfb0214d7ac08ed0d2d9ca920ea6d4f4be2654ea5" // Arbitrum Upgrades Multisig
 
 async function main() {
 	const wallet = new ethers.Wallet(process.env.DEPLOYER_PRIVATEKEY, ethers.provider)
