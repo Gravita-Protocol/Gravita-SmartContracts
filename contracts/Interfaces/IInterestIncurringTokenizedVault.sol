b6be3bc6fcc2c51bb8a0d7f0cd8a0cada207622d
gbirckan <gbirckan@gmail.com>
2023-12-10 13:07:22 -0300

    natspec update

--- contracts/Interfaces/IInterestIncurringTokenizedVault.sol
+++ contracts/Interfaces/IInterestIncurringTokenizedVault.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
+/**
+ * @dev Modifications to this interface will lead to corresponding changes in its interfaceId. 
+ *      This interfaceId is utilized in introspection by both the ActivePool and the StabilityPool to identify 
+ *      the necessity of unwrapping tokens. Therefore, it is crucial to remember to upgrade both of these 
+ *      contracts if any alterations are made here.
+ */
 interface IInterestIncurringTokenizedVault is IERC4626 {
 	
 	function collectInterest() external;
